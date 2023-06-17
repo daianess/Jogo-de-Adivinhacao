241e605639e5ae739040a8b32cf801920dccc6d9
Daiane da S. Santos <daianesadm@outlook.com>
2023-06-17 10:13:50 -0300

    Condição de acerto ou erro.

--- adivinhacao.c
+++ adivinhacao.c
@@ -6,8 +6,31 @@ int main()
   printf("* Bem-vido ao Jogo de Adivinhação *\n");
   printf("*************************************\n");
 
+  int numeroSecreto = 42;
+
   int chute;
   printf("Qual é o seu chute? ");
   scanf("%d", &chute);
-  printf("Você chutou o número %d!", chute);
+  printf("Você chutou o número %d!\n", chute);
+
+ int acertou = chute == numeroSecreto;
+  if (acertou)
+  {
+    printf("Parabéns! Você acertou.\n");
+    printf("Jogue de novo, você é um bom jogador!\n");
+  }
+  else
+  {
+    printf("Que pena, você errou.\n");
+    printf("Mas não desanime, tente de novo!\n");
+
+    if (chute > numeroSecreto)
+    {
+      printf("Seu chute foi maior do que o número secreto!\n");
+    }
+    if (chute < numeroSecreto)
+    {
+      printf("Seu chute foi menor do que o número secreto!\n");
+    }
+  }
 }
